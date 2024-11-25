dc5881054a66623a70a235a02a751ee70167dd5f
Frank Steven Piedrahita <163359733+Fpiedrahitap@users.noreply.github.com>
2024-11-25 14:01:39 -0500

    final

---
+++ login-02/js/login.js
@@ -0,0 +1,4 @@
+document.getElementById('login-button').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    window.location.href = '../mi-app-gastos/add-gastos.html';
+});
